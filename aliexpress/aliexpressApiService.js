9936db86d9b25586447869f15eb56e368fd1bedc
Alon Segal <anialonsegal@gmail.com>
2025-04-20 15:25:24 +0300

    feat: add getHotProducts method with options and corresponding tests

--- aliexpress/aliexpressApiService.js
+++ aliexpress/aliexpressApiService.js
@@ -73,4 +73,94 @@ module.exports = class AliexpressApiService {
             'tracking_id': this.trackingId,
         });
     }
+
+    /**
+     * Get hot products from AliExpress
+     * @param {Object} options - Query options
+     * @param {string} [options.category_ids] - List of category IDs
+     * @param {string} [options.fields] - Response parameter list (e.g. 'commission_rate,sale_price')
+     * @param {string} [options.keywords] - Filter products by keywords
+     * @param {number} [options.max_sale_price] - Filter products by highest price (in cents)
+     * @param {number} [options.min_sale_price] - Filter products by lowest price (in cents)
+     * @param {number} [options.page_no=1] - Page number
+     * @param {number} [options.page_size=20] - Record count of each page (1-50)
+     * @param {string} [options.platform_product_type] - Product type: ALL,PLAZA,TMALL
+     * @param {string} [options.sort] - Sort by: SALE_PRICE_ASC, SALE_PRICE_DESC, LAST_VOLUME_ASC, LAST_VOLUME_DESC
+     * @param {string} [options.target_currency] - Target currency (e.g. USD, GBP, CAD, EUR)
+     * @param {string} [options.target_language] - Target language (e.g. EN, RU, PT, ES)
+     * @param {string} [options.delivery_days] - Estimated delivery days (3, 5, 7, 10)
+     * @param {string} [options.ship_to_country] - Ship to country code
+     * @param {string} [options.promotion_name] - Promotion name
+     * @returns {Promise}
+     */
+    getHotProducts(options = {}) {
+        const defaultOptions = {
+            page_no: '1',
+            page_size: '20',
+            target_currency: 'USD',
+            target_language: 'EN',
+            ship_to_country: 'US'
+        };
+
+        const params = {
+            'method': 'aliexpress.affiliate.hotproduct.query',
+            'app_signature': this.appSignature,
+            'tracking_id': this.trackingId,
+            ...defaultOptions,
+            ...options
+        };
+
+        // Convert numeric values to strings if they exist
+        if (params.max_sale_price !== undefined) params.max_sale_price = String(params.max_sale_price);
+        if (params.min_sale_price !== undefined) params.min_sale_price = String(params.min_sale_price);
+        if (params.page_no !== undefined) params.page_no = String(params.page_no);
+        if (params.page_size !== undefined) params.page_size = String(params.page_size);
+
+        return this.client.callAPI('', params);
+    }
+
+    /**
+     * Get products from AliExpress
+     * @param {Object} options - Query options
+     * @param {string} [options.category_ids] - List of category IDs
+     * @param {string} [options.fields] - Response parameter list (e.g. 'commission_rate,sale_price')
+     * @param {string} [options.keywords] - Filter products by keywords
+     * @param {number} [options.max_sale_price] - Filter products by highest price (in cents)
+     * @param {number} [options.min_sale_price] - Filter products by lowest price (in cents)
+     * @param {number} [options.page_no=1] - Page number
+     * @param {number} [options.page_size=20] - Record count of each page (1-50)
+     * @param {string} [options.platform_product_type] - Product type: ALL,PLAZA,TMALL
+     * @param {string} [options.sort] - Sort by: SALE_PRICE_ASC, SALE_PRICE_DESC, LAST_VOLUME_ASC, LAST_VOLUME_DESC
+     * @param {string} [options.target_currency] - Target currency (e.g. USD, GBP, CAD, EUR)
+     * @param {string} [options.target_language] - Target language (e.g. EN, RU, PT, ES)
+     * @param {string} [options.delivery_days] - Estimated delivery days (3, 5, 7, 10)
+     * @param {string} [options.ship_to_country] - Ship to country code
+     * @param {string} [options.promotion_name] - Promotion name
+     * @returns {Promise}
+     */
+    getProducts(options = {}) {
+        const defaultOptions = {
+            page_no: '1',
+            page_size: '20',
+            target_currency: 'USD',
+            target_language: 'EN',
+            ship_to_country: 'US'
+        };
+
+        const params = {
+            'method': 'aliexpress.affiliate.product.query',
+            'app_signature': this.appSignature,
+            'tracking_id': this.trackingId,
+            ...defaultOptions,
+            ...options
+        };
+
+        // Convert numeric values to strings if they exist
+        if (params.max_sale_price !== undefined) params.max_sale_price = String(params.max_sale_price);
+        if (params.min_sale_price !== undefined) params.min_sale_price = String(params.min_sale_price);
+        if (params.page_no !== undefined) params.page_no = String(params.page_no);
+        if (params.page_size !== undefined) params.page_size = String(params.page_size);
+
+        return this.client.callAPI('', params);
+    }
 }
--- aliexpress/aliexpressApiService.test.js
+++ aliexpress/aliexpressApiService.test.js
@@ -48,4 +48,34 @@ describe('aliexpressApiService', () => {
             expect(aliexpress.client.callAPI.mock.calls.pop()).toMatchSnapshot();
         }); 
     });
+
+    describe('getHotProducts', () => {
+        it('should call getPromisified with the correct params', () => {
+            const aliexpress = new AliexpressApiService('key', 'secret', 'signature', 'trackingId');
+            const options = {
+                category_ids: '123,456',
+                fields: 'commission_rate,sale_price',
+                keywords: 'phone',
+                max_sale_price: 10000,
+                min_sale_price: 1000,
+                page_no: 1,
+                page_size: 20,
+                platform_product_type: 'ALL',
+                sort: 'SALE_PRICE_DESC',
+                target_currency: 'USD',
+                target_language: 'EN',
+                delivery_days: '7',
+                ship_to_country: 'US',
+                promotion_name: 'Summer Sale'
+            };
+            aliexpress.getHotProducts(options);
+            expect(aliexpress.client.callAPI.mock.calls.pop()).toMatchSnapshot();
+        });
+
+        it('should call getPromisified with default params when no options provided', () => {
+            const aliexpress = new AliexpressApiService('key', 'secret', 'signature', 'trackingId');
+            aliexpress.getHotProducts();
+            expect(aliexpress.client.callAPI.mock.calls.pop()).toMatchSnapshot();
+        });
+    });
 });
